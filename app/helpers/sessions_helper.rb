2dea1ed0957ffb853a06161886dd8d8083cbb85c
cof.iland <shima4113@gmail.com>
2021-09-22 05:08:49 +0000

    user login

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,22 @@
+module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+    #下記コードと同じ内容
+    #if @current_user
+    #  return @current_user
+    #else
+    #  @current_user = User.find_by(id: session[:user_id])
+    #  return @current_user
+  end
+  
+    #loginでtrue,していなければfalse
+  def logged_in?
+    #インスタンスが存在していれば!(not)インスタンス=false,!false=trueとなる
+    !!current_user
+    #下記と一緒
+    #if current_user
+    #return true
+    #else
+    #return false
+  end
+    
